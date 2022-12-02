71f6c533d3d619261ae3247ac71f1623c5b8acd0
alynek <alyne0x67@gmail.com>
2022-12-01 21:57:51 -0300

    Create ElasticConfiguration class

---
+++ ElasticsearchCrud/ElasticConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Elastic.Clients.Elasticsearch;
+using Elastic.Transport;
+using Microsoft.Extensions.Configuration;
+
+namespace ElasticsearchCrud
+{
+    public static class ElasticConfiguration
+    {
+        public static void AddElasticsearch(this IServiceCollection services, IConfiguration configuration)
+        {
+            ElasticsearchClientSettings settings = 
+                new ElasticsearchClientSettings(new Uri(configuration["ElasticsearchSettings:uri"]))
+            .Authentication(new BasicAuthentication(configuration["ElasticsearchSettings:login"], configuration["ElasticsearchSettings:password"]));
+
+            var client = new ElasticsearchClient(settings);
+
+            services.AddSingleton(client);
+        }
+    }
+}
